90466e8be99ae1fa8fb575f0341a5be54ff94193
kimbaqueen <k.a.starr@live.com>
2022-06-25 15:19:24 -0600

    step 4 done

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -17,4 +17,22 @@ test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
-})
+});
+
+test('Choices div shows up when Draw button clicked', async () => {
+    await driver.findElement(By.id('draw')).click();
+    const choices = await driver.findElement(By.id('choices'));
+
+    const isDisplayed = await choices.isDisplayed();
+    await driver.sleep(5000);
+    expect(isDisplayed).toBeTruthy;
+});
+
+test('player-duo div displys when Add to Duo button clicked', async () => {
+    await driver.findElement(By.xpath("//button[contains(@class,'bot-btn')]")).click();
+    const playerDuo = await driver.findElement(By.xpath("//div[contains(@class, 'player-duo')]"));
+
+    const isDisplayed = await playerDuo.isDisplayed();
+    await driver.sleep(5000);
+    expect(isDisplayed).toBeTruthy;
+});
