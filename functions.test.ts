9aaa9ac2e1b1c4e44f805dc3a2397bbc3d284652
kimbaqueen <k.a.starr@live.com>
2022-06-25 14:36:43 -0600

    step 3 completed

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,14 @@
-const {shuffleArray} = require('./utils')
+const { shuffleArray } = require('./utils')
 
 describe('shuffleArray should', () => {
     // CODE HERE
-})
+    test('be random', () => {
+        const testArray = [2, 9, 0, 5, 3];
+        expect(testArray).not.toBe([2, 9, 0, 5, 3]);
+    });
+
+    test('be a copied array', () => {
+        const array = [2, 9, 0, 5, 3];
+        expect(array).toEqual(array);
+    });
+});
